c2a64989a93282adadeb83580fa146d6cc4603b8
louisoduor <louisodhiambo20@gmail.com>
2023-12-12 17:57:18 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet) {
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+    // Write your code here!
+    if (feet <= 400) {
+      return "This one is on me!";
+    } else if (feet > 2000 && feet <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else if (feet > 2500) {
+      return "No can do.";
+    } else {
+      return "This is an invalid distance.";
+    }
+  }
+  
+  function ternaryCheckCity(city) {
+    // Write your code here!
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+  function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    switch (tip) {
+      case "generous":
+        return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
+  }
